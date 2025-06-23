ab27ef905698f252d0dbf4b80923c284bef2c098
Milk-Cool <coder.tomsk@gmail.com>
2025-06-23 19:07:36 +0300

    sessions

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import expressSession from "express-session";
+import connectPgSimple from "connect-pg-simple";
+import { pool } from "./index.js";
+const pgSession = connectPgSimple(expressSession);
 
 const port = 5328;
 
 const app = express();
+app.use(expressSession({
+    store: new pgSession({
+        pool,
+        createTableIfMissing: true
+    }),
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    cookie: { maxAge: 10e12 }
+}));
 
 app.get("/test", (_, res) => res.send("Hello from express!"));
 
